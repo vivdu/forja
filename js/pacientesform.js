d7c7d251d42cbac4b4657f2cae0e05e4360a142e
Viviana Duarte <yvduartef@gmail.com>
2022-05-23 02:53:56 -0500

    fixed form validation

--- js/pacientesform.js
+++ js/pacientesform.js
@@ -1,26 +1,79 @@
 
-let form=document.getElementById("form-usuario");
-let campos=querySelectorAll("#form-usuario input");
+let form=document.getElementById("frm-usuario");
+let campos=document.querySelectorAll("#frm-usuario input");
 
 const reglas={
     textos: /^[a-zA-Z-AÁ-ÿ\s]{1,50}$/,
-    numeros: /^\d{7-10}$/,
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.]+$/,
+    numeros: /^[\+]?[(]?[0-9]{4,10}$/im,
+    correo: /\S+@\S+/,
+}
+const inputs={
+    nombre:false,
+    apellido:false,
+    idnumero:false,
+    telefono:false,
+    correo:false,
 }
 
-form.addEventListener("submit",e=>{alert("Se envió el formulario")});
- 
-let validarCampos=(e)=>{    
-console.log("evento input")
-console.log(e.target.name)
-switch(e.target.name) {
-    case "numdoc":
-        if(reglas.numeros.test(e.target.value)){
-            document.getElementById("g-numdoc").classList.remove
-        }
-        else {
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+    console.log(inputs.nombre);
+    console.log(inputs.apellido);
+    console.log(inputs.idnumero);
+    console.log(inputs.correo);
+    console.log(inputs.telefono);
+    if(inputs.nombre && inputs.apellido && inputs.idnumero && inputs.telefono && inputs.correo){
+        alert("Registro existoso");
+    }
+    else{
+        alert("No se envió el formulario, revise que todos los campos estén diligenciados");
+        document.getElementById("frm-no-enviado").classList.add("frm-no-enviado-visible");
+    } 
+})
 
-        }
+let validarCampos=(e)=>{
+    
+let validarInput=(regla,grupo,input)=>{
+    if(regla.test(input.value)){
+        document.querySelector(".form-item").classList.remove('error');
+        document.querySelector(".form-item").classList.add('success');
+        
+        document.querySelector(".form-item").classList.remove('fa-xmark');
+        
+        inputs[grupo]=true;
+    }else{
+        document.querySelector(".form-item").classList.add('error'); 
+        document.querySelector(".form-item").classList.remove('success');
+        document.querySelector(".form-item").classList.remove('fa-circle-check');
+        inputs[grupo]=false;
+    }
 }
+
+    switch(e.target.name){
+        
+        case "nombre":
+            validarInput(reglas.textos,e.target.name,e.target);
+        break;
+        case "apellido":
+            validarInput(reglas.textos,e.target.name,e.target);
+        break;
+        case "direccion":
+            validarInput(reglas.textos,e.target.name,e.target);
+        break;
+        case "telefono":
+            validarInput(reglas.numeros,e.target.name,e.target);
+        break;
+        case "idnumero":
+            validarInput(reglas.numeros,e.target.name,e.target)
+        break;
+        case "correo":
+            validarInput(reglas.correo,e.target.name,e.target);
+        break;
+    }
 }
-campos.forEach((campo))
+
+campos.forEach((campo)=>{
+    campo.addEventListener('keyup',validarCampos);
+    campo.addEventListener('blur',validarCampos);
+})
+
